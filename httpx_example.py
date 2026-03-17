de3a94c0c95ce009a528ffa29037c93edd9051b8
pavelGavrilenko <train.000@yandex.ru>
2026-03-17 14:51:27 +0200

    Введение в HTTPX.Работаем с API /api/v1/users через HTTPX

---
+++ httpx_example.py
@@ -0,0 +1,15 @@
+import httpx
+
+try:
+    response = httpx.get("https://jsonplaceholder.typicode.com/invalid-url")
+    response.raise_for_status()  # Вызовет исключение при 4xx/5xx
+except httpx.HTTPStatusError as e:
+    print(f"Ошибка запроса: {e}")
+
+
+with httpx.Client() as client:
+    response1 = client.get("https://jsonplaceholder.typicode.com/todos/1")
+    response2 = client.get("https://jsonplaceholder.typicode.com/todos/2")
+
+print(response1.json())  # Данные первой задачи
+print(response2.json())  # Данные второй задачи

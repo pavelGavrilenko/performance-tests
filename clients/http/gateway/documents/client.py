5498f876a225464860d9868ff7c9f9ee46ecfb1e
pavelGavrilenko <train.000@yandex.ru>
2026-03-24 12:26:00 +0200

    Практикуемся в использовании HTTP API клиентов.Практика использования HTTP API-клиентов: получение документов по счету

--- clients/http/gateway/documents/client.py
+++ clients/http/gateway/documents/client.py
@@ -1,7 +1,23 @@
 from httpx import Response
-
+from typing import TypedDict
 from clients.http.client import HTTPClient
 from clients.http.gateway.client import build_gateway_http_client  # Импортируем builder
+
+
+class DocumentDict(TypedDict):
+    """
+    Описание структуры документов.
+    """
+    url: str
+    document: str
+
+
+class GetTariffDocumentResponseDict(TypedDict):
+    tariff: DocumentDict
+
+
+class GetContractDocumentResponseDict(TypedDict):
+    tariff: DocumentDict
 
 
 class DocumentsGatewayHTTPClient(HTTPClient):
@@ -26,6 +42,14 @@ class DocumentsGatewayHTTPClient(HTTPClient):
         :return: Ответ от сервера (объект httpx.Response).
         """
         return self.get(f"/api/v1/documents/contract-document/{account_id}")
+
+    def get_tariff_document(self, account_id: str) -> GetTariffDocumentResponseDict:
+        response = self.get_tariff_document_api(account_id)
+        return response.json()
+
+    def get_contract_document(self, account_id: str) -> GetContractDocumentResponseDict:
+        response = self.get_contract_document_api(account_id)
+        return response.json()
 
 
 def build_documents_gateway_http_client() -> DocumentsGatewayHTTPClient:

fa0d025c580f94e7323907a1359dec03d4020f8c
pavelGavrilenko <99758732+pavelGavrilenko@users.noreply.github.com>
2026-03-28 17:23:40 +0200

    Знакомство с Faker.Применение Fake в Pydantic-моделях

--- clients/http/gateway/users/schema.py
+++ clients/http/gateway/users/schema.py
@@ -1,4 +1,6 @@
 from pydantic import BaseModel, Field, EmailStr, ConfigDict
+from tools.fakers import fake
+
 
 
 # Добавили суффикс Schema вместо Dict
@@ -29,11 +31,16 @@ class CreateUserRequestSchema(BaseModel):  # Наследуем от BaseModel �
     """
     model_config = ConfigDict(populate_by_name=True)
 
-    email: EmailStr
-    last_name: str = Field(alias="lastName")  # Использовали alise
-    first_name: str = Field(alias="firstName")  # Использовали alise
-    middle_name: str = Field(alias="middleName")  # Использовали alise
-    phone_number: str = Field(alias="phoneNumber")  # Использовали alise
+    # Добавили генерацию случайного email
+    email: EmailStr = Field(default_factory=fake.email)
+    # Добавили генерацию случайной фамилии
+    last_name: str = Field(alias="lastName", default_factory=fake.last_name)
+    # Добавили генерацию случайного имени
+    first_name: str = Field(alias="firstName", default_factory=fake.first_name)
+    # Добавили генерацию случайного отчества
+    middle_name: str = Field(alias="middleName", default_factory=fake.middle_name)
+    # Добавили генерацию случайного номер телефона
+    phone_number: str = Field(alias="phoneNumber", default_factory=fake.phone_number)
 
 
 # Добавили суффикс Schema вместо Dict

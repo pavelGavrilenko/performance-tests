5a17cdd8aad6a2458c7da78d422d9158b389c31b
pavelGavrilenko <train.000@yandex.ru>
2026-04-23 18:06:58 +0300

    Введение в grpcio.Практика работы с grpcio

---
+++ grpcio_open_debit_card_account.py
@@ -0,0 +1,42 @@
+import grpc
+
+from contracts.services.gateway.users.rpc_create_user_pb2 import CreateUserRequest, CreateUserResponse
+from contracts.services.gateway.users.users_gateway_service_pb2_grpc import UsersGatewayServiceStub
+from contracts.services.gateway.accounts.accounts_gateway_service_pb2_grpc import AccountsGatewayServiceStub
+from contracts.services.gateway.accounts.rpc_open_debit_card_account_pb2 import \
+    OpenDebitCardAccountRequest, OpenDebitCardAccountResponse
+
+# Используем генератор фейковых данных, созданный ранее
+from tools.fakers import fake
+
+# Устанавливаем соединение с gRPC-сервером по адресу localhost:9003
+channel = grpc.insecure_channel("localhost:9003")
+
+# Создаём gRPC-клиент для UsersGatewayService
+users_gateway_service = UsersGatewayServiceStub(channel)
+# Создаём gRPC-клиент для ccountsGatewayService
+accounts_gateway_service = AccountsGatewayServiceStub(channel)
+
+# Формируем запрос на создание пользователя с рандомными данными
+create_user_request = CreateUserRequest(
+    email=fake.email(),
+    last_name=fake.last_name(),
+    first_name=fake.first_name(),
+    middle_name=fake.middle_name(),
+    phone_number=fake.phone_number()
+)
+
+# Отправляем запрос и получаем ответ
+create_user_response: CreateUserResponse = users_gateway_service.CreateUser(create_user_request)
+print('Create user response:', create_user_response)
+
+# Формируем запрос на создание карты для созданого пользователя
+open_debit_request = OpenDebitCardAccountRequest(
+    user_id=create_user_response.user.id
+)
+
+# Отправляем запрос и получаем ответ
+open_debit_response: OpenDebitCardAccountResponse = accounts_gateway_service.OpenDebitCardAccount(
+    open_debit_request
+)
+print('Open debit card account response:', open_debit_response)
